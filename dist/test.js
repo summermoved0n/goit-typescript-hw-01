0205032984b47c410fed0f6348c8641b1c0de7e6
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-02-07 10:40:11 -0500

    test

--- dist/test.js
+++ dist/test.js
@@ -1,11 +1,9 @@
+"use strict";
 // function add(num1: number, num2: number) {
 //   return num1 + num2;
 // }
-
 // add(1, 1);
-
-let total: number = 100;
-let firstName: string = "Bob";
-let isActive: boolean = false;
-
+let total = 100;
+let firstName = "Bob";
+let isActive = false;
 firstName = 5;
